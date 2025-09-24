7a691057c2cbf732e57cfa5f47d47058941d0a8d
abhayabikramshahi <abhayabikramshahiofficial@gmail.com>
2025-09-24 12:11:25 +0545

    file uploaded

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <h1>Midnight MVP Version</h1>
+        <button>profile</button>
+      </div>
+      <p>Home</p>
+    </>
+  );
+}
+
+export default App;
